cdfabe577a6a3a830d55d5bf694b56c09a53d20d
uraido <79716183+uraido@users.noreply.github.com>
2023-04-15 16:58:32 -0300

    Captures both Youtube chat messages and the user's microphones input simultaneously, without overlap.

--- YoutubeLiveComments.py
+++ YoutubeLiveComments.py
@@ -1,22 +1,40 @@
 import pytchat
+from AikoSpeechInterface import *
+from threading import Thread
 
 # Set livestream ID here
-chat = pytchat.create(video_id="wclpOmBi5u8")
+chat = pytchat.create(video_id="3TsfnhTLvBI")
 
-niggatext = ''
-niggalist = ''
+def aiko_listen():
+    global is_aiko_talking
 
-while True:
+    while True:
+        text = listen()
+        if text == None:
+            continue
+        if not is_aiko_talking:
+            is_aiko_talking = True
+            print('Captured microphone input:')
+            print(text)
+            say(text)
+            is_aiko_talking = False
+            
+
+def youtube_chat():
+    global chat
+    global is_aiko_talking
+
     while chat.is_alive():
         for c in chat.get().sync_items():
-            print(f"{c.message}")
-            niggatext = c.message
-        print('here!')
-        if niggatext == 'code red':
-            niggatext = ''
-            break
-            
-    # while        
-    print('AIKOOOOOOOOOOOOOOOOOO')
-    if niggatext == '':
-        break
+            if not is_aiko_talking:
+                is_aiko_talking = True
+                print('Captured chat message:')
+                print(c.message)
+                say(c.message)
+                is_aiko_talking = False
+
+if __name__ == "__main__":
+    is_aiko_talking = False
+
+    Thread(target=aiko_listen).start()
+    Thread(target=youtube_chat).start()

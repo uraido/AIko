14d0ac253c74fffd988d7bfd7ec18b909729d064
uraido <79716183+uraido@users.noreply.github.com>
2023-04-15 22:38:03 -0300

    Broken ass code that does not work (is_broken!)

--- YoutubeLiveComments.py
+++ YoutubeLiveComments.py
@@ -9,10 +9,58 @@ prompt_list = []
 
 is_broken = False
 
+'''
+def aiko_listen_while_talking():
+    global is_aiko_talking
+    global is_broken
+    global prompt_list
+
+    while True:
+        if is_broken:
+            break
+        if is_aiko_talking:
+            text = listen()
+            if text == None:
+                continue
+            if text[:3].lower() == 'hey':
+                prompt_list.append(text)
+                print(prompt_list)'''
+
+
+# test function
+
+def banana():
+    global is_broken
+
+    while True:
+        if is_broken:
+            break
+        print('banana')
+
+
+def aiko_read_while_talking():
+    global is_aiko_talking
+    global is_broken
+    global prompt_list
+
+    while chat.is_alive():
+        if is_broken:
+            break
+        if is_aiko_talking:
+            text = listen()
+            if text[:3].lower() == 'hey':
+                print('Updated prompt_list:')
+                prompt_list.append(text)
+                print(prompt_list)
+            for c in chat.get().sync_items():
+                print('Updated prompt_list:')
+                prompt_list.append(c.message)
+                print(prompt_list)
 
 def aiko_listen():
     global prompt_list
     global is_aiko_talking
+    global is_broken
 
     while True:
         if is_broken:
@@ -22,10 +70,8 @@ def aiko_listen():
             continue
         if is_aiko_talking:
             continue
-        else:
-            if text[:3].lower() == 'hey':
-                prompt_list.append(text)
-                print(prompt_list)
+        if text in prompt_list:
+            continue
         if text[:3].lower() == 'hey':
             is_aiko_talking = True
             print('Captured microphone input:')
@@ -34,11 +80,11 @@ def aiko_listen():
             is_aiko_talking = False
 
 
-def youtube_chat():
+def aiko_read():
     global chat
     global is_aiko_talking
-    global prompt_list
     global is_broken
+    global prompt_list
 
     while chat.is_alive():
         if is_broken:
@@ -46,16 +92,16 @@ def youtube_chat():
         for c in chat.get().sync_items():
             if c.message.lower() == 'code red':
                 is_broken = True
+                print('Breaking...')
                 break
+            if c.message in prompt_list:
+                continue
             if not is_aiko_talking:
                 is_aiko_talking = True
                 print('Captured chat message:')
                 print(c.message)
                 say(c.message)
                 is_aiko_talking = False
-            else:
-                prompt_list.append(c.message)
-                print(prompt_list)
             
         
         
@@ -64,4 +110,7 @@ if __name__ == "__main__":
     is_aiko_talking = False
 
     Thread(target=aiko_listen).start()
-    Thread(target=youtube_chat).start()
+    Thread(target=aiko_read).start()
+    Thread(target=aiko_read_while_talking).start()
+    Thread(target=aiko_listen_while_talking).start()
+    #Thread(target=banana).start()

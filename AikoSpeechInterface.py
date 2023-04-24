8f2c4325dcc488f99488b0aa43b0fc3cf4bafd9f
uraido <79716183+uraido@users.noreply.github.com>
2023-04-24 14:28:07 -0300

    AikoSpeechInterface.py: Integrated my_push_to_talk.py

--- AikoSpeechInterface.py
+++ AikoSpeechInterface.py
@@ -1,6 +1,6 @@
 """
 AikoSpeechInterface.py (former TextToSpeech.py)
-Version 3.3
+Version 3.4
 
 Library of text to speech functions for Aiko.
 
@@ -18,8 +18,7 @@ pip install:
 - openai
 
 Changelog:
-- Renamed generate_mp3 functions to generate_tts
-- Added whisperAPI speech to text option.
+- Integrated push to talk functions from my_push_to_talk.py
 """
 
 import gtts                     # text to mp3 file
@@ -28,6 +27,10 @@ from pydub import AudioSegment  # to increase text-to-speech audio pitch
 from elevenlabslib import *     # elevenlabs API python integration
 import speech_recognition as sr # google speech-to-text
 import openai                   # whisperAPI speech to text
+import wave                     # to write .wav files
+import pyaudio                  # to record audio
+from pynput import keyboard     # for push to talk hotkey
+from time import sleep
 
 # elevenlabs voicetype. this is a list because multiple voices can have the same name
 try:
@@ -159,6 +162,103 @@ def generate_stt_whisperAPI(filename : str):
 
     return(transcript.text)
 
+# ---------------------------------------- PUSH TO TALK SECTION --------------------------------------------------
+
+hotkey = 'o' # push to talk hotkey. DON'T SET IT TO 'P' OR '0'.
+
+# bools to keep track of key presses and the recording
+
+key_pressed = False
+done_recording = False
+
+def on_press(key):
+    global key_pressed
+    global hotkey
+
+    if not 'char' in dir(key):
+        return
+
+    elif key.char != hotkey:
+        return
+
+    if not key_pressed:
+    
+        key_pressed = True
+        print('start recording')
+
+def on_release(key):
+    global key_pressed
+    global done_recording
+    global hotkey
+
+    if not 'char' in dir(key):
+        return
+
+    #if key.char != hotkey:
+        #return
+
+    if key_pressed:
+        key_pressed = False
+        done_recording = True
+        print('stopped recording')
+
+def start_push_to_talk():
+    global key_pressed
+    global hotkey
+    global done_recording
+
+    # sets and starts recording related variables
+
+    CHUNK = 8192
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    frames = []
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+
+    done_recording = False
+
+    while True:
+
+        # collect keyboard events
+
+        listener = keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release)
+        listener.start()
+
+        # saves audio frames while key is pressed
+
+        while key_pressed:
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        if done_recording:
+
+            # ends audio stream
+
+            stream.stop_stream()
+            stream.close()
+            p.terminate()
+
+            # saves recording into wav file
+
+            wf = wave.open('recording.wav', 'wb')
+            wf.setnchannels(CHANNELS)
+            wf.setsampwidth(p.get_sample_size(FORMAT))
+            wf.setframerate(RATE)
+            wf.writeframes(b''.join(frames))
+            wf.close()
+
+            # generates stt and returns it
+            stt_output = generate_stt_whisperAPI('recording.wav')
+
+            return stt_output
+
+        sleep(0.1)
+
 if __name__ == "__main__":
 
     # for testing whisperAPI stt function
@@ -180,3 +280,12 @@ if __name__ == "__main__":
     # for testing elevenlabs voice
 
     #say("Hello Rchart-Kun!", elevenlabs=True)
+
+    # to test push to talk
+
+    while True:
+        stt = start_push_to_talk()
+        print(stt)
+
+    if 'code red' in stt.lower():
+        break

aeb7cac4b81bb980f83f85e1212d5e5aa365c0a0
uraido <79716183+uraido@users.noreply.github.com>
2023-06-12 17:09:36 -0300

    VoiceLink: Added STT functions.

--- VoiceLink.py
+++ VoiceLink.py
@@ -1,8 +1,26 @@
+"""
+Voicelink.py
+
+Handles voice related functionality such as text to speech and speech to text for Aiko's scripts.
+
+File requirements:
+- AikoINIhandler.py >= 1.2
+
+pip install:
+- azure.cognitiveservices.speech
+- keyboard
+
+Changelog:
+
+040:
+- Added speech to text functionality through start_speech_recognition() and stop_speech_recognition().
+"""
 import os
 import azure.cognitiveservices.speech as speechsdk
 import subprocess
+import keyboard
+import time
 from configparser import ConfigParser
-import os
 
 if __name__ == '__main__':
     from AikoINIhandler import handle_ini
@@ -64,12 +82,88 @@ audio_config = speechsdk.audio.AudioOutputConfig(
 # sets the voice
 speech_config.speech_synthesis_voice_name = azure_voice #'en-AU-CarlyNeural' #'en-GB-MaisieNeural' #'en-US-AnaNeural'
 
-def say(text : str):
-    global speech_config
-    global audio_config
+# builds SpeechSynthesizer class
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-    speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+def say(text : str):
+    global speech_synthesizer
+
     speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
 
+# builds SpeechRecognizer class
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+
+# sets variable for handling speech recognizing loop
+recognition_activated = False
+
+def start_speech_recognition(parse_func, hotkey : str = 'Page Up'):
+    """
+    Starts continuous speech recognition using Azure Speech to Text.
+
+    Parameters:
+    - hotkey (str): The hotkey to toggle speech recognition on/off. Default is 'Page Up'.
+    - parse_func: The function to parse and handle recognized speech events.
+
+    Usage:
+    - Make sure to call this function in a threaded environment for reliable control over the while loop.
+    - To stop the continuous recognition, you can call the stop_continuous_recognition() function from another thread.
+    """
+    global recognition_activated
+    global speech_recognizer
+
+    recognition_activated = True
+
+    speech_recognizer.start_continuous_recognition()
+    speech_recognizer.recognized.connect(lambda evt: parse_func(evt))
+
+    is_recognizing = True
+
+    while recognition_activated:
+        if keyboard.is_pressed(hotkey) and is_recognizing:
+            speech_recognizer.stop_continuous_recognition()
+            is_recognizing = False
+            print('\nPaused continuous speech recognition.\n')
+        elif keyboard.is_pressed(hotkey) and not is_recognizing:
+            speech_recognizer.start_continuous_recognition()
+            print('\nResumed continuous speech recognition.\n')
+            is_recognizing = True
+        time.sleep(0.1)
+
+def stop_speech_recognition():
+    global recognition_activated
+
+    recognition_activated = False
+    print('\nDeactivated speech recognition.\n')
+
 if __name__ == '__main__':
+    from VoiceLink import start_speech_recognition, stop_speech_recognition
+    from threading import Thread
+
+    # tests say function
     say('Hello there!')
+
+    # sets up a function to be called on the speech_recognizer.recognized event
+    def parse_event(evt):
+        event = str(evt)
+
+        keyword = 'text="'
+        stt_start = event.index(keyword)
+        stt_end = event.index('",')
+        
+        message = event[stt_start + len(keyword):stt_end]
+
+        if message != '':
+            print(message)
+
+    # to test speech recognition. started in a thread.
+    def speech_recognition_thread():
+        start_speech_recognition(parse_func = parse_event)
+
+    # to handle stopping the speech recognition.
+    def stop_recognition_thread():
+        keyboard.wait('space')
+        stop_speech_recognition()
+
+    # starts threads
+    Thread(target=speech_recognition_thread).start()
+    Thread(target=stop_recognition_thread).start()

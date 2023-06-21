d4113a3783fa4d5ac20191153997d1780bcfc29e
uraido <79716183+uraido@users.noreply.github.com>
2023-06-21 03:03:12 -0300

    AIko: Added messageList class for handling context

--- AIko.py
+++ AIko.py
@@ -2,43 +2,29 @@
 AIko.py
 
 Requirements:
-- AikoSpeechInterface.py (5.0 or greater) and its requirements
+- VoiceLink.py and its requirements
 - AikoINIhandler.py
 
 pip install:
 - openai
-
-pip3 install:
-- pytimedinput   
+- func_timeout
 
 txt files:
 - AIko.txt
-- time_out_prompts.txt
 - key_openai.txt
 - scenarios.txt
 
 Changelog:
 
-100alpha:
-- Interactions are now fully handed by the new AIko class.
-- txt_to_string() function now returns an empty string instead of None when an error occurs.
-- Many dynamic features, such as silence breaker and scenarios have been temporalily removed. They need to be
-adapted to work with the latest changes before they can be reimplemented.
-
-101alpha:
-- Reimplemented scenarios.
-- Implemented side prompts, which can be added using the add_side_prompt AIko method.
-- Using non timeout version of generate_gpt_completion function for now, since the timeout version is throwing
-errors.
-
-102alpha:
-- AIko.interact() method now has a use_system_role parameter.
-- AIko.interact()'s username parameter is now optional.
+110alpha:
+- Added messageList class for handling information such as context lists and side prompts.
+- The scenario is no longer a public attribute of the AIko class. To change it, one must use the change_scenario()
+method.
 ===============================================================================================================================
 """ 
 
 # PLEASE set it if making a new build. for logging purposes
-build_version = ('Aiko102alpha').upper() 
+build_version = ('Aiko110alpha').upper() 
 
 print(f'{build_version}: Starting...')
 print()
@@ -50,7 +36,7 @@ if __name__ == '__main__':
 # ----------------- Imports -----------------
 
 import openai                          # gpt3
-from VoiceLink import say    # text to speech function
+from VoiceLink import say              # text to speech function
 from datetime import datetime          # for logging
 from pytimedinput import timedInput    # input with timeout
 from random import choice              # random
@@ -88,13 +74,13 @@ session_token_usage = 0
 
 # ------------------------------------------- functions -----------------------------------------------
 
-def create_context_list(dummy_object : "any" = '', length : int = context_slots):
+def create_limited_list(length : int = context_slots):
   '''
   Returns a limited list of empty strings.
   '''
   new_list = []
   for i in range(length):
-    new_list.append(dummy_object)
+    new_list.append('')
   return new_list
 
 def txt_to_string(filename: str):
@@ -196,6 +182,58 @@ def txt_to_list(txt_filename : str):
   
   return lines_list
 # ----------------------------------- end of functions ------------------------------------------------
+class messageList:
+  """
+  A class that holds a limited list of messages meant for prompting GPT.
+
+  Parameters:
+      slots (int): The maximum number of slots the list can hold.
+
+  Methods:
+      add_item(item : str, role : str):
+          Removes the oldest item in the list and appends a new one.
+      get_items() -> list:
+          Returns the populated items of the list.
+      is_empty() -> bool:
+          Whether items have been added to the list or not.
+  """
+  def __init__(self, slots : int = context_slots):
+    self.__message_list__ = create_limited_list(slots)
+
+  def __str__(self):
+    items = self.get_items()
+    items_as_strings = []
+
+    for item in items:
+      items_as_strings.append(str(item))
+
+    return '\n'.join(items_as_strings)
+
+  def add_item(self, item : str, role : str):
+    """
+      Removes the oldest item in the list and appends a new one.
+
+      Attributes:
+          item (str): The message to be added.
+          role (str): The role the message will be under when requesting a completion. Can be either "system",
+          "user" or "assistant".
+    """
+    self.__message_list__.pop(0)
+    self.__message_list__.append({"role": role, "content": item})
+
+  def get_items(self) -> list:
+    """
+      Returns the populated items of the list.
+    """
+    populated_items = []
+
+    for item in self.__message_list__:
+      if item != '':
+        populated_items.append(item)
+    return populated_items
+
+  def is_empty(self) -> bool:
+    return self.__message_list__[-1] == ''
 
 class AIko:
   """
@@ -204,46 +242,38 @@ class AIko:
     Attributes:
         character_name (str): The name of the AI character.
         personality_file (str): The filename of the personality file.
-        __personality__ (str): The personality text loaded from the personality file.
-        __log__ (str): The filename of the log file.
-        __context__ (list): A list of user and assistant messages representing the conversation context.
+
+    Parameters:
+        scenario (str): The scenario in which the character currently finds itself in.  
 
     Methods:
-        __init__(self, character_name: str, personality_filename: str):
-            Initializes an instance of the AIko class.
-        
-        __create_log__(self):
-            Creates a log file and reports initial information.
-        
-        __update_log__(self, user_string: str, completion_data: tuple):
-            Updates the log file with the user's input and the generated output.
-        
-        interact(self, username: str, message: str):
+        interact(username: str, message: str):
             Interacts with the AI character by providing a username and a message.
+        add_side_prompt(side_prompt : str):
+            Injects a side prompt into the character's memory.
+        def change_scenario(scenario : str):
+            Changes the current scenario.
     """
 
   def __init__(self, character_name : str, personality_filename : str, scenario : str = ''):
-    """
-    Initializes an instance of the AIko class.
-
-    Args:
-        character_name (str): The name of the AI character.
-        personality_filename (str): The filename of the personality file.
-    """
     self.character_name = character_name
     self.personality_file = personality_filename
-    self.scenario = scenario
+
     self.__personality__ = txt_to_string(personality_filename)
     self.__log__ = self.__create_log__()
-    self.__context__ = create_context_list(('', ''))
-    self.__side_prompts__ = create_context_list()
+
+    self.__context__ = messageList()
+    self.__side_prompts__ = messageList(5)
+
+    self.__scenario__ = messageList(1)
+    self.__scenario__.add_item(scenario, "system")
 
   def __create_log__(self):
     """
-    Creates a log file and reports initial information.
+      Creates a log file and reports initial information.
 
-    Returns:
-        log_filename (str): The filename of the created log file.
+      Returns:
+          log_filename (str): The filename of the created log file.
     """
     global build_version
 
@@ -269,11 +299,11 @@ class AIko:
 
   def __update_log__(self, user_string : str, completion_data : tuple):
     """
-    Updates the log file with the user's input and the generated output.
+      Updates the log file with the user's input and the generated output.
 
-    Args:
-        user_string (str): The user's input message.
-        completion_data (tuple): A tuple containing the generated output and token usage information.
+      Args:
+          user_string (str): The user's input message.
+          completion_data (tuple): A tuple containing the generated output and token usage information.
     """
     global session_token_usage
 
@@ -292,62 +322,55 @@ class AIko:
 
   def interact(self, message : str, username : str = 'User', use_system_role : bool = False):
     """
-    Interacts with the AI character by providing a username and a message.
-
-    Args:
-        message (str): The message sent by the user.
-        username (str): The username.
-        use_system_role (bool): Whether to use the system role instead of the user role when prompting the user's
-        message.
+      Interacts with the AI character by providing an username and a message.
     """
-    messages = [{"role":"system", "content": self.__personality__},]
-    if self.scenario != '':
-      messages.append({"role":"system", "content": self.scenario})
+    messages = [{"role":"system", "content": self.__personality__}]
 
-    # parses side prompts and adds them to the messages
-    if self.__side_prompts__[-1] != '':
-      for side_prompt in self.__side_prompts__:
-        if side_prompt != '':
-          messages.append({"role":"system", "content": side_prompt})
+    messages += self.__scenario__.get_items()
+    messages += self.__side_prompts__.get_items()
+    messages += self.__context__.get_items()
 
-    # parses context list and adds interactions to the messages
-    if self.__context__[-1] != ('', ''):
-      for context in self.__context__:
-        if context != ('', ''):
-          messages.append({"role":"user", "content": context[0]})
-          messages.append({"role":"assistant", "content": context[1]})
-
-    # adds user prompt to the messages
     if use_system_role:
       messages.append({"role":"system", "content": message})
     else:
       messages.append({"role":"user", "content": f'{username}: {message}'})
 
-    # requests completion
     completion = generate_gpt_completion(messages)
     print(completion[0])
 
-    # parses completion to be fed to text to speech
-    #if f'{self.character_name}:' in completion[0][:len(self.character_name) + 2]:
-      #say(completion[0][len(self.character_name) + 1:])
-    # otherwise just voices it if no parsing is needed
-    #else:
-      #say(completion[0])
+    # parses completion (when appliable) to be fed to text to speech
+    if f'{self.character_name}:' in completion[0][:len(self.character_name) + 2]:
+      say(completion[0][len(self.character_name) + 1:])
 
-    # updates context list with latest interaction
-    self.__context__.pop(0)
-    self.__context__.append((f'{username}: {message}', completion[0]))
+    else:
+      say(completion[0])
+
+    if use_system_role:
+      self.__context__.add_item(completion[0], "assistant")
+    else:
+      self.__context__.add_item(f'{username}: {message}', "user")
+      self.__context__.add_item(completion[0], "assistant")
 
     self.__update_log__(message, completion)
 
-  def add_side_prompt(self, side_prompt):
-    self.__side_prompts__.pop(0)
-    self.__side_prompts__.append(side_prompt)
+  def add_side_prompt(self, side_prompt : str):
+    """
+      Injects a side prompt into the character's memory.
+    """
+    self.__side_prompts__.add_item(side_prompt, "system")
+
+  def change_scenario(self, scenario : str):
+    """
+      Changes the current scenario.
+    """
+    self.__scenario__.add_item(scenario, "system")
+
 
 if __name__ == "__main__":
   username = config.get('GENERAL', 'username')
-  scenario = choice(txt_to_list('prompts\scenarios.txt'))
-  aiko = AIko('Aiko', 'prompts\AIko.txt', scenario)
+  scenarios = txt_to_list('prompts\scenarios.txt')
+  aiko = AIko('Aiko', 'prompts\AIko.txt', choice(scenarios))
+  aiko.add_side_prompt('Aiko prefers cats over dogs. Especially siamese cats.')
   while True:
     message = input(f'{username}: ')
     if breaker.lower() in message.lower():

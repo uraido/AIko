eae5b5ce1810a63e174e8a2de842f4ceadf748d0
Ricardo Jara <35924585+Rchart@users.noreply.github.com>
2023-11-02 20:31:11 -0300

    Change match for ifs

--- AikoSentiment.py
+++ AikoSentiment.py
@@ -1,3 +1,12 @@
+'''
+AikoSentiment.py
+
+pip install:
+- azure-ai-textanalytics
+
+'''
+
+
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient
 
@@ -12,13 +21,14 @@ def sentiment_analysis(text: str):
 
     result = text_analytics_client.analyze_sentiment(texts)
     sentiment = result[0].sentiment
-    match sentiment:
-        case 'positive':
-            score = int(result[0].confidence_scores.positive * 100)
-        case 'neutral':
-            score = int(result[0].confidence_scores.neutral * 100)
-        case 'negative':
-            score = int(result[0].confidence_scores.negative * 100)
+    #match sentiment:
+    if sentiment == 'positive':
+        #case 'positive':
+        score = int(result[0].confidence_scores.positive * 100)
+    elif sentiment == 'neutral':
+        score = int(result[0].confidence_scores.neutral * 100)
+    elif sentiment == 'negative':
+        score = int(result[0].confidence_scores.negative * 100)
 
     return sentiment, score
 
